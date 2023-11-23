d95fd2648c7af23cb2585d0d7046b71a84f42bc6
Ernesto Portillo <portillo.ernesto@outlook.com>
2023-11-23 04:06:59 +0000

    First prompt-ui app

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/manifoldco/promptui"
+)
+
+
 
 func main() {
+	promt := promptui.Select{
+		Label: "Select the person",
+		Items: []string{"John", "Wick", "Lord", "Voldemort"},
+	}
+	i, res, err := promt.Run()
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
 	fmt.Println("Hello world")
+	fmt.Printf("You choose %q\n", res)
+	fmt.Printf("Number returned %v\n", i)
 }
