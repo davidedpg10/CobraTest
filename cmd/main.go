221d2d8ba048d8a560a3a9cb027c6bed65fb29c6
Ernesto Portillo <portillo.ernesto@outlook.com>
2023-11-23 05:22:15 +0000

    Build system integrated

--- cmd/main.go
+++ cmd/main.go
@@ -6,19 +6,15 @@ import (
 	"github.com/manifoldco/promptui"
 )
 
-
-
 func main() {
 	promt := promptui.Select{
 		Label: "Select the person",
 		Items: []string{"John", "Wick", "Lord", "Voldemort"},
 	}
-	i, res, err := promt.Run()
+	_, res, err := promt.Run()
 	if err != nil {
 		fmt.Printf("Prompt failed %v\n", err)
 		return
 	}
-	fmt.Println("Hello world")
 	fmt.Printf("You choose %q\n", res)
-	fmt.Printf("Number returned %v\n", i)
 }
